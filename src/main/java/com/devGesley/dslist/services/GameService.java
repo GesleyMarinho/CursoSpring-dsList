b071ab540f46eeffa9a88791924e2b8a0d7bda35
Gesley Marinho <gesleymarinho@gmail.com>
2025-05-20 21:15:25 -0300

    Get por ID

--- src/main/java/com/devGesley/dslist/services/GameService.java
+++ src/main/java/com/devGesley/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devGesley.dslist.dto.GameDTO;
 import com.devGesley.dslist.dto.GameMinDTO;
 import com.devGesley.dslist.entities.Game;
 import com.devGesley.dslist.repositories.GameRepository;
@@ -15,9 +17,17 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
+	}
+
+	public GameDTO findById(long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+
 	}
 }
